444a97d9966c4a9d5e41bb48db5c6e5696e630b7
João Pedro <joaopedro3000.sa@gmail.com>
2025-01-21 21:30:41 -0300

    Adicionando a pasta de testes.

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,11 @@
+describe('template spec', () => {
+  it('passes', () => {
+    cy.visit('https://adopet-frontend-cypress.vercel.app/');
+    cy.contains('a', 'Cadastrar').click(); //procurar o elemento "a" com o texto "cadastrar" e clicar.
+    cy.get('input[name="nome"]').type('João Pedro'); //pegar o "input" com name "nome" e escrever "João Pedro".
+    cy.get('input[name="email"]').type('teste@teste.com');
+    cy.get('input[name="password"]').type('Comida@123');
+    cy.get('input[name="confirm_password"]').type('Comida@123');
+    cy.contains('button', 'Cadastrar').click();
+  })
+})
